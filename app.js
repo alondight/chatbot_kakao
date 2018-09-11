eda23492c55c31bd1b2b463cc92e08d2e285f157
박준혁 <alondightoo@naver.com>
2018-09-11 10:42:59 +0900

    'TEST'

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+const http = require('http');
+const app = express();
+
+
+app.get('/keyboard' ,  function(req,res) {
+	var data = {
+		'type' : 'buttons',
+		'buttons' : ['과일','채소','정보']
+	};
+	res.json(data);
+});
+
+
+http.createServer(app).listen(9090,function() {
+	 console.log('서버 실행 중');
+});
